53f487e0f17988434b80d77739951fd217001223
zemuuv <samuvarg@gmail.com>
2025-10-02 00:09:59 -0500

    Backend
    se genera la estructura del archivo backend donde ira la api y logica

---
+++ RestauranteKonrad/src/main/java/com/proyecto/restaurantekonrad/RestauranteKonradApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.restaurantekonrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteKonradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteKonradApplication.class, args);
+    }
+
+}
---
+++ RestauranteKonrad/src/test/java/com/proyecto/restaurantekonrad/RestauranteKonradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.restaurantekonrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteKonradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
